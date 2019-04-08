9f0482599887adc665cbdc311f715fc71b52536f
David Ford <dford@stonedoorgroup.com>
2019-04-08 15:20:20 -0600

    This is to change the message to be in properties

--- example1/load-all.sh
+++ example1/load-all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-oc login 10.2.2.2:8443 -u example -p pass
+oc login https://console.ocp-a.zu2.nonprod.travelport.io --token=AR-FSDZz0AxJg2Qmhl9LBN4c_oZfTdxKbzRvwpXftPg
 oc new-project configmap-example
 oc create -f configmap-example.json
 oc create -f node-app-deployment.json
