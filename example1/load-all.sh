927fa597c637e79d5c83c64ed6dbb09b5bf52c74
David Ford <dford@stonedoorgroup.com>
2019-04-09 16:29:33 -0600

    changed json files for install on clusters

--- example1/load-all.sh
+++ example1/load-all.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-oc login https://console.ocp-a.zu2.nonprod.travelport.io --token=AR-FSDZz0AxJg2Qmhl9LBN4c_oZfTdxKbzRvwpXftPg
 oc new-project configmap-example
 oc create -f configmap-example.json
-oc create -f node-app-deployment.json
-oc create -f node-app-build.json
+oc create -f node-app-service.json
+oc create -f node-app-dc.json
